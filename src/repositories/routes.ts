4e0fd511fdfabfc8748fe359337606158bdd3008
Marco <marcosiegman01@gmail.com>
2021-03-03 18:26:45 -0600

    add: cv to project

--- src/repositories/routes.ts
+++ src/repositories/routes.ts
@@ -3,6 +3,6 @@ export type Route = { url: string; name: string; isExternal: boolean };
 export const routes: Route[] = [
     { url: "/portfolio", name: "Portfolio", isExternal: false },
     { url: "/#about", name: "About", isExternal: false },
-    { url: "/#cv", name: "Download CV", isExternal: true },
+    { url: "/assets/cv.pdf", name: "Download CV", isExternal: true },
 ];
 
